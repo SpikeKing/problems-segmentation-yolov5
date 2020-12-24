fe7af135f2da1a2df6cd7f4ab8d85a1bb325f296
zhengsheng.wcl <zhengsheng.wcl@alibaba-inc.com>
2020-12-24 10:25:03 +0800

    update

--- preprocess/data_preprocess.py
+++ preprocess/data_preprocess.py
@@ -61,13 +61,13 @@ class DataPreprocess(object):
             data_lines += sub_lines
         print('[Info] 文本行数: {}'.format(len(data_lines)))
 
-        pool = Pool(processes=80)
+        # pool = Pool(processes=80)
         for idx, data_line in enumerate(data_lines):
-            # DataPreprocess.process_line(data_line, out_file)
-            pool.apply_async(DataPreprocess.process_line, (idx, data_line, out_file))
+            DataPreprocess.process_line(idx, data_line, out_file)
+            # pool.apply_async(DataPreprocess.process_line, (idx, data_line, out_file))
 
-        pool.close()
-        pool.join()
+        # pool.close()
+        # pool.join()
         print('[Info] 处理完成: {}'.format(out_file))
 
 
