c846714e4ca31173c9c92a174ca0e2a3d084e5a2
zhengsheng.wcl <zhengsheng.wcl@alibaba-inc.com>
2021-02-18 21:04:40 +0800

    update

--- scripts/sample_processor.py
+++ scripts/sample_processor.py
@@ -11,15 +11,19 @@ import cv2
 from multiprocessing.pool import Pool
 from myutils.project_utils import *
 from root_dir import DATA_DIR
-from x_utils.vpf_utils import get_problem_segmentation_vpf_service, get_problem_segmentation_cv_vpf_service
+from myutils.cv_utils import *
+from x_utils.vpf_utils import get_problem_segmentation_cv_vpf_service
 
 
 class SampleProcessor(object):
     def __init__(self):
-        self.input_file = os.path.join(DATA_DIR, 'page_dataset_raw',
-                                       'sanghu.zj_question_cut_sampled_kousuanpigai_url_3w_0104')
-        print('[Info] input_file: {}'.format(self.input_file))
-        self.out_dir = os.path.join(DATA_DIR, '整页拍_out')
+        self.input_file1 = os.path.join(DATA_DIR, 'page_dataset_raw',
+                                        'sanghu.zj_question_cut_sampled_kousuanpigai_url_3w_0104')
+        self.input_file2 = os.path.join(DATA_DIR, 'page_dataset_raw',
+                                        'sanghu.zj_question_cut_sampled_jueying_url_5k_1229')
+        print('[Info] input_file1: {}'.format(self.input_file1))
+        print('[Info] input_file2: {}'.format(self.input_file2))
+        self.out_dir = os.path.join(DATA_DIR, 'full_page_imgs_1024')
         mkdir_if_not_exist(self.out_dir)
 
     @staticmethod
@@ -35,22 +39,26 @@ class SampleProcessor(object):
         # res_dict = get_problem_segmentation_vpf_service(img_url)
         # oss_url_out1 = res_dict['data']['oss_url_out2']
 
-        res_dict = get_problem_segmentation_cv_vpf_service(img_url)
-        oss_url = res_dict['data']['oss_url']
+        # res_dict = get_problem_segmentation_cv_vpf_service(img_url)
+        # oss_url = res_dict['data']['oss_url']
 
-        print('[Info] out_url: {}'.format(oss_url))
-        is_ok, img_bgr = download_url_img(oss_url)
+        print('[Info] img_url: {}'.format(img_url))
+        is_ok, img_bgr = download_url_img(img_url)
         out_path = os.path.join(out_dir, img_name)
+
+        img_bgr = resize_img_fixed(img_bgr, 1024, is_height=True)
 
         cv2.imwrite(out_path, img_bgr)
         print('[Info] 处理完成 out_path: {}'.format(out_path))
 
     def process(self):
-        data_lines = read_file(self.input_file)
+        data_lines1 = read_file(self.input_file1)
+        data_lines2 = read_file(self.input_file2)
+        data_lines = data_lines1 + data_lines2
         random.seed(47)
         random.shuffle(data_lines)
 
-        # data_lines = data_lines[0:200]
+        # data_lines = data_lines[0:10]
         print('[Info] 样本数: {}'.format(len(data_lines)))
 
         paths_list, names_list = traverse_dir_files(self.out_dir)  # 用于避免重复生产
@@ -62,7 +70,8 @@ class SampleProcessor(object):
 
         p.close()
         p.join()
-        print('[Info] 处理完成: {}'.format(self.input_file))
+        print('[Info] 处理完成: {}'.format(self.input_file1))
+        print('[Info] 处理完成: {}'.format(self.input_file2))
 
 
 def main():
